4532b9e9b1381413f385338f1dd39f242ea00af5
Andrey Nazarov <andreynaz@abv.bg>
2019-04-02 06:42:14 +0300

    Add project files.

---
+++ ISITask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISITask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
